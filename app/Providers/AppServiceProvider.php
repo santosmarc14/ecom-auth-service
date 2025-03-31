7cf116a72f7be0e5ad2cd6a94f40ea130d7a2ba7
Marc Benedict Santos <s.marcbenedict@gmail.com>
2025-03-31 12:26:23 +0800

    feat: token expiration extended to 1 month

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::enablePasswordGrant();
-
+        Passport::tokensExpireIn(now()->addDays(30));
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
     }
 }
